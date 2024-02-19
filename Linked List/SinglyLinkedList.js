3b2aa07fa5f16c838102cd2d7472e8139de708f3
Mateus <mattoss.barreto@gmail.com>
2024-02-19 13:19:03 -0300

    added unshift method to singlyLinkedList

--- Linked List/SinglyLinkedList.js
+++ Linked List/SinglyLinkedList.js
@@ -34,4 +34,17 @@ class LinkedList {
         return this;
     }
 
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        };
+        this.length++;
+        return this;
+    }
+
 }
